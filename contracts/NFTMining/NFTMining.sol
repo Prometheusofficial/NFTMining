125b595b0a7a3b4f8f76cf09753794b6c78feafd
Sam <John.Smith.Ed@outlook.com>
2022-01-25 15:51:55 +0800

    git update all

---
+++ contracts/NFTMining/NFTMining.sol
@@ -0,0 +1,432 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../interface/IERC721.sol";
+
+import "../interface/IPancakeRouter.sol";
+import "../interface/IBattleNFT.sol";
+import "../interface/IGodNFT.sol";
+import "../utils/Manageable.sol";
+
+interface IConfigMining{
+    function getCheckListLength(uint256 _gaimID) external view returns(uint256);
+    function getAttributesVaule(uint256 _gaimID,uint256 _index) external view returns(uint256 _Attribute,uint256 _vaule);
+    function isValidGameID(uint256 _gaimID) external view returns(bool);
+}
+
+contract NFTMining is Manageable{
+    
+    using SafeMath for *;
+    using SafeERC20 for IERC20;
+    
+    //IBattleNFT public batterNFT = IBattleNFT(0x378aC3870Ff0D0d28308e934a666a52752b55DB8);
+    //IGodNFT public godNFT = IGodNFT(0xd34Eb2d530245a60C6151B6cfa6D247Ee92668c7);
+    //IERC20 public rewardToken = IERC20(0x03aC6AB6A9a91a0fcdec7D85b38bDFBb719ec02f);
+    //IPancakeRouter public router = IPancakeRouter(0x10ED43C718714eb63d5aA57B78B54704E256024E);
+    //IERC20 public USDT = IERC20(0x55d398326f99059fF775485246999027B3197955); // this is USDT
+    //test
+    IPancakeRouter public router = IPancakeRouter(0x10ED43C718714eb63d5aA57B78B54704E256024E);
+    IBattleNFT public batterNFT = IBattleNFT(0x3Bd00BBD07E18743bFfb7589E2eE89FFd695f642);
+    IGodNFT public godNFT = IGodNFT(0xD4fD679fA138589e81148bb2Dac6f0E8631e404e);
+    IERC20 public rewardToken = IERC20(0xE36339cC77A7b155d7BC0543223D97831e5F60E9); // this is mga 
+    IERC20 public USDT = IERC20(0x55d398326f99059fF775485246999027B3197955); // this is USDT
+    uint256 public deadlineDelta = 5 minutes;
+    
+    IConfigMining public configMining;
+    
+    bytes4 internal constant MAGIC_ON_ERC721_RECEIVED = 0x150b7a02;
+    
+    mapping(uint256 => poolStruct) public poolInfo;
+    mapping(uint256 => mapping(address => userStruct)) public userInfo;
+    
+    uint256 public alreadWithdraw;
+    uint256 public maxRewardAmount = 30000000*1e18;
+    uint256 public basePerBlock = 1*1e14;
+    uint256 public maxGain = 64;
+    uint256 public userNeedReward; // this is add use used reward amount;
+    
+    bool public isNeedSwap;
+    address[] public swapPath;
+    
+    
+    uint256 public ListingID;
+    mapping(uint256 => ListInfoS) public listing; // ID=INFO
+    
+    mapping(address => uint256[]) public userPools;
+    
+    event AddPool(address user,uint256 _pid,uint256 _gameID,uint256 _perBlock,uint256 _startBlock,uint256 _endBlock);
+    event Depost(address _user,uint256 _pid,uint256 _godTokenID,uint256[]  _batterTokens); 
+    event Withdraw(address _user,uint256 _pid);
+    event WithdrawReward(address _user,uint256 _pid,uint256 _reward);
+    
+    struct userStruct{
+        uint256 startBlock;
+        uint256 witdrawBlock;
+        uint256 godTokenID;
+        uint256[] batterTokenIDList;
+    }
+    
+    struct ListInfoS{
+        IERC20  rewardToken;
+        uint256  maxRewardAmount;
+        uint256 basePerBlock;
+        uint256 userNeedReward;
+        uint256 alreadWithdraw;
+        address[] path;
+        bool needSwap;
+    }
+    struct poolStruct{
+        address user;
+        uint256 gameID;
+        uint256 perBlock;
+        uint256 startBlock;
+        uint256 endBlock;
+        uint256 stopBlock;
+        uint256 rewardAmount;
+    }
+    
+    modifier onlyUserPool(uint256 _pid,address _user){
+        require(poolInfo[_pid].user == _user,"only user pool");
+        _;
+    }
+    
+    constructor(IConfigMining _configMining,bool _isNeedSwap) {
+        configMining = _configMining;
+        
+        if(_isNeedSwap){
+            isNeedSwap = _isNeedSwap;
+            swapPath.push(address(rewardToken));
+            swapPath.push(address(USDT));
+        }else{
+            rewardToken = USDT;
+        }
+        
+    }
+    
+    function setConfigMining(IConfigMining _configMining) public onlyManager{
+        configMining = _configMining;
+    }
+    
+    function addPool(address _user,uint256 _pid,uint256 _gameID,uint256 _BlockLen) public onlyManager{
+        require(configMining.isValidGameID(_gameID),"only config gameID");
+        require(userNeedReward < maxRewardAmount,"only have reward");
+        poolStruct storage pss = poolInfo[_pid];
+        require(pss.user == address(0),"alread create ");
+        
+        poolInfo[_pid] = poolStruct({
+            user:_user,
+            gameID:_gameID,
+            perBlock:basePerBlock,
+            startBlock:block.number,
+            endBlock:block.number+_BlockLen,
+            stopBlock:0,
+            rewardAmount:0
+        });
+        
+        userPools[_user].push(_pid);
+         
+        emit AddPool(_user,_pid,_gameID,basePerBlock,block.number,block.number+_BlockLen);
+    }
+    
+    function getRemainingReward() public view returns(uint256){
+        return maxRewardAmount.sub(userNeedReward);
+    }
+    
+    function depost(uint256 _pid,uint256 _godTokenID,uint256[] memory _batterTokens) public onlyUserPool(_pid,msg.sender){
+        require(_batterTokens.length == 3,"_batterTokens not enough");
+        poolStruct memory pss = poolInfo[_pid];
+        require(block.number < pss.endBlock,"ended stake");
+        
+        userStruct storage uss = userInfo[_pid][msg.sender];
+        
+        uss.startBlock = block.number > pss.startBlock ? block.number:pss.startBlock;
+        uss.witdrawBlock = uss.startBlock;
+        uss.godTokenID = _godTokenID;
+        IERC721 nftGod = IERC721(address(godNFT));
+        nftGod.safeTransferFrom(msg.sender,address(this), _godTokenID);
+        
+        uint256 len = _batterTokens.length;
+        IERC721 nftbatter = IERC721(address(batterNFT));
+        for(uint256 i=0; i<len ;i++){
+            uss.batterTokenIDList.push(_batterTokens[i]);
+            nftbatter.safeTransferFrom(msg.sender,address(this),_batterTokens[i]);
+        }
+        
+        userNeedReward = userNeedReward.add(calcUserNeedReward(_pid,pss.gameID,msg.sender,uss.startBlock,pss.endBlock));
+        
+        emit Depost(msg.sender,_pid,_godTokenID,_batterTokens);
+    }
+    
+    // this need  check maxReward 
+    function calcUserNeedReward(
+        uint256 _pid,
+        uint256 _gameID,
+        address _user,
+        uint256 _startBlock,
+        uint256 _endBlock) public view  returns(uint256 _needReward){
+            uint256 blockLen = _endBlock.sub(_startBlock);
+           
+            userStruct memory uss = userInfo[_pid][_user];
+            poolStruct memory pss = poolInfo[_pid];
+        
+            uint256 gain = (calcUserGodGain(uss.godTokenID)+1).mul(calcUserBattleGain(_pid,_gameID,_user));
+            gain = gain > maxGain ? maxGain:gain;
+            _needReward  = blockLen.mul(pss.perBlock).mul(gain);
+    }
+    
+    function withdraw(uint256 _pid) public{
+        
+        userStruct storage uss = userInfo[_pid][msg.sender];
+        require(checkWithdraw(_pid,msg.sender),"checkWithdraw fail");
+        
+        withdrawReward(_pid);
+        
+        IERC721 nftGod = IERC721(address(godNFT));
+        nftGod.safeTransferFrom(address(this), msg.sender, uss.godTokenID);
+        uss.godTokenID = 0;
+        uint256 len = uss.batterTokenIDList.length;
+        IERC721 nftbatter = IERC721(address(batterNFT));
+        for(uint256 i=0; i<len ;i++){
+            nftbatter.safeTransferFrom(address(this),msg.sender,uss.batterTokenIDList[i]);
+        }
+        for(uint256 i=len-1; i<=0 ;i--){
+            uss.batterTokenIDList.pop();
+            if(i==0){
+                break;
+            }
+            
+        }
+        
+        emit Withdraw(msg.sender,_pid);
+        
+    }
+    
+    function EmergencyWithdraw(uint256 _pid) public onlyUserPool(_pid,msg.sender){
+        userStruct storage uss = userInfo[_pid][msg.sender];
+        
+        IERC721 nftGod = IERC721(address(godNFT));
+        nftGod.safeTransferFrom(address(this), msg.sender, uss.godTokenID);
+        uss.godTokenID = 0;
+        uint256 len = uss.batterTokenIDList.length;
+        IERC721 nftbatter = IERC721(address(batterNFT));
+        for(uint256 i=0; i<len ;i++){
+            nftbatter.safeTransferFrom(address(this),msg.sender,uss.batterTokenIDList[i]);
+        }
+        for(uint256 i=len-1; i<=0 ;i--){
+            uss.batterTokenIDList.pop();
+            if(i==0){
+                break;
+            }
+            
+        }
+        uss.witdrawBlock = block.number;
+    }
+    
+    function userPoolsLength(address _user) public view returns(uint256){
+        return userPools[_user].length;
+    }
+    
+    function checkWithdraw(uint256 _pid,address _user) public view returns(bool _canWithdraw){
+        poolStruct memory pss = poolInfo[_pid];
+        userStruct memory uss = userInfo[_pid][_user];
+        
+        
+        if(block.number >= pss.endBlock && uss.godTokenID > 0 && uss.batterTokenIDList.length>0){
+            _canWithdraw = true;
+        }
+        
+    }
+    
+    function withdrawReward(uint256 _pid) public onlyUserPool(_pid,msg.sender) {
+        address user = msg.sender;
+        require(checkWithdraw(_pid,user),"checkWithdraw fail");
+        poolStruct storage pss = poolInfo[_pid];
+        userStruct storage uss = userInfo[_pid][user];
+        uint256 _reward = pendingReward(_pid,pss.gameID,user);
+        
+        if(_reward > 0 ){
+            
+            uss.witdrawBlock = block.number;
+            
+            safeTransferReward(msg.sender,_reward);
+            
+            alreadWithdraw = alreadWithdraw.add(_reward);
+            
+            pss.rewardAmount = pss.rewardAmount.add(_reward);
+        }
+        
+        emit WithdrawReward(msg.sender,_pid,_reward);
+    }
+    
+    function safeTransferReward(address _user,uint256 _reward) internal {
+        address contractAddress = address(this);
+        uint256 curRewardBalance = rewardToken.balanceOf(contractAddress);
+        require(curRewardBalance > _reward,"not enght");
+        if(isNeedSwap){
+            uint256 swapPathLen = swapPath.length;
+            uint256 beforBalance = IERC20(swapPath[swapPathLen-1]).balanceOf(contractAddress);
+            swap(rewardToken,_reward);
+            uint256 afterBalance = IERC20(swapPath[swapPathLen-1]).balanceOf(contractAddress);
+            require(afterBalance.sub(beforBalance) > 0,"swap balance is zeor");
+            
+            IERC20(swapPath[swapPathLen-1]).transfer(_user,afterBalance.sub(beforBalance));
+            
+        }else{
+            rewardToken.transfer(_user,_reward);
+        }
+        
+    }
+    
+    function swap(IERC20 _token,uint256 _amount) internal returns(uint256 amountOut){
+        _token.approve(address(router), 0);
+        _token.approve(address(router), _amount);
+
+        uint256[] memory amounts = router.getAmountsOut(_amount, swapPath);
+        amounts = router.swapExactTokensForTokens(
+            _amount,
+            amounts[amounts.length - 1],
+            swapPath,
+            address(this),
+            block.timestamp.add(deadlineDelta)
+        );
+        
+        amountOut = amounts[amounts.length - 1];
+    }
+    
+    function pendingReward(uint256 _pid,uint256 _gameID,address _user) public view returns(uint256 _reward){
+        userStruct memory uss = userInfo[_pid][_user];
+        poolStruct memory pss = poolInfo[_pid];
+        uint256 sBlock = uss.startBlock > uss.witdrawBlock ? uss.startBlock:uss.witdrawBlock;
+        if(block.number > sBlock && block.number <= pss.endBlock){
+            uint256 eBlock = block.number >= pss.endBlock?  pss.endBlock :block.number;
+            uint256 blockLen = eBlock.sub(sBlock);
+            uint256 gain = (calcUserGodGain(uss.godTokenID)+1).mul(calcUserBattleGain(_pid,_gameID,_user));
+            gain = gain > maxGain ? maxGain:gain;
+            _reward  = blockLen.mul(pss.perBlock).mul(gain);
+        }
+        
+    }
+    
+    function calcUserBattleGain(uint256 _pid,uint256 _gameID,address _user) public view returns(uint256 ){
+        uint256 x ;
+       
+        userStruct memory uss = userInfo[_pid][_user];
+        uint256 len = uss.batterTokenIDList.length;
+        for(uint256 i=0; i<len ;i++){
+            x = x + calcBattleGain(_gameID,uss.batterTokenIDList[i]);
+            if(x>=3){
+                break;
+            }
+        }
+        
+        return 2**x;
+    }
+    
+    // this returns is 2 ** n
+    function calcBattleGain(uint256 _gameID,uint256 _tokenID) public view returns(uint256){
+        // 1 Generation 6
+    // 2 HP 8-0
+    // 3 ATK 8-1
+    // 4 SPD 8-2
+    // 5 TEC 8-3
+    // 6 DEF 8-4 
+    // 7 SkillAmount 7 
+    // 8 Skill 7
+        IBattleNFT.CAttributes_S memory bss = batterNFT.cAttributes(_tokenID);
+        uint256 len = configMining.getCheckListLength(_gameID);
+        uint256 attributes;
+        uint256 arvaule;
+        for(uint256 i = 0; i<len ; i++){
+            
+            (attributes,arvaule) = configMining.getAttributesVaule(_gameID,i);
+            
+            if(attributes == 1){
+              // Generation 
+              if(bss.generation >= arvaule){
+                  return 1;
+              }
+            }else if(attributes == 2){
+               // 2 HP 8-0
+               if(bss.battleAttrs[0] >= arvaule){
+                  return 1;
+               }
+            }else if(attributes == 3){
+                // 3 ATK 8-1
+                if(bss.battleAttrs[1] >= arvaule){
+                  return 1;
+               }
+            }else if(attributes == 4){
+                // 4 SPD 8-2
+                if(bss.battleAttrs[2] >= arvaule){
+                  return 1;
+                }
+            }else if(attributes == 5){
+                // 5 TEC 8-3
+                if(bss.battleAttrs[3] >= arvaule){
+                  return 1;
+                }
+            }else if(attributes == 6){
+                // 6 DEF 8-4 
+                if(bss.battleAttrs[4] >= arvaule){
+                  return 1;
+                }
+            }else if(attributes == 7){
+                // 7 SkillAmount 7
+                if(bss.skillIds.length >= arvaule){
+                  return 1;
+                }
+            }else if(attributes == 8){
+                // 8 Skill 7
+                for(uint256 j=0; j<bss.skillIds.length;j++ ){
+                   if(bss.skillIds[j] == arvaule){
+                       return 1;
+                   } 
+                }
+                
+            }
+        }
+        return 0;
+    }
+    function onERC721Received(address _operator, address _from,uint256 _tokenId,bytes calldata _data) external pure returns(bytes4){
+        return MAGIC_ON_ERC721_RECEIVED;
+    }
+    
+    // this returns 1,3 7
+    function calcUserGodGain(uint256 _godTokenID) public view returns(uint256 godGain){
+        IGodNFT.CAttributes_S memory css;
+        css = godNFT.cAttributes(_godTokenID);
+        if(css.rarityIdx == 2001){
+            godGain = 1;
+        }else if(css.rarityIdx == 2002){
+            godGain = 2;
+        }else if(css.rarityIdx == 2003){
+            godGain = 7;
+        }
+    }
+    
+    function initListing() internal{
+        ListingID = 1;
+        address[]  memory p;
+        
+        ListInfoS memory s1  = ListInfoS({
+            rewardToken : IERC20(0xE36339cC77A7b155d7BC0543223D97831e5F60E9), // usdt 
+            maxRewardAmount : 10000*1e18,
+            basePerBlock : 1*1e14,
+            userNeedReward :0,
+            alreadWithdraw :0 ,
+            path :p,
+            needSwap : false
+        });
+        listing[1] = s1;
+    }
+    
+}
---
+++ contracts/NFTMining/configMining.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "../interface/IBattleNFT.sol";
+import "../interface/IGodNFT.sol";
+import "../utils/Manageable.sol";
+
+
+contract ConfigMining is Manageable{
+    uint256  public totalGameID;
+    
+    // 1 Generation 6
+    // 2 HP 8-0
+    // 3 ATK 8-1
+    // 4 SPD 8-2
+    // 5 TEC 8-3
+    // 6 DEF 8-4 
+    // 7 SkillAmount 7 
+    // 8 Skill 7
+    /*struct CAttributes_S {
+        uint256 nameIdx;        //1
+        uint256 campIdx;        //2 1001 Japanese_God
+        uint256 cardIdx;        //3
+        uint256 jobIdx;         //4
+        uint256 classLevel;     //5
+        uint256 attrIdx;        //6
+        uint256 generation;     //7 init 1, can not set directly
+        uint256[] skillIds;     //8
+        uint256[] battleAttrs;  //9 0 hp, 1 atk, 2 spd, 3, tec, 4 def, 5 mov 
+        uint256[] extendsAttrs;
+    }*/
+    
+    struct AStruct{
+        uint256 attribute;
+        uint256 vaule;
+    }
+    mapping(uint256 => AStruct[]) public miningAttributesList;
+    mapping(uint256 => bool) public isSetGameID;
+    
+    
+    function setCheckList(uint256 _gameID,uint256[] memory _Attributes,uint256[] memory _vaules) public onlyManager{
+        uint256 len = _Attributes.length;
+        require(_vaules.length == len,"not equel length");
+        totalGameID += 1;
+        for(uint256 i=0 ; i<len; i++){
+            miningAttributesList[_gameID].push(AStruct({
+                attribute:_Attributes[i],
+                vaule:_vaules[i]
+            }));
+        }
+        
+        isSetGameID[_gameID] = true;
+        
+    }
+    
+    function updateCheckList(uint256 _gaimID,uint256 _Attribute,uint256 _vaule) public onlyManager{
+        uint256 len = miningAttributesList[_gaimID].length;
+        AStruct memory ams = AStruct({
+            attribute:_Attribute,
+                vaule:_vaule
+        });
+        
+        for(uint256 i=0 ; i<len; i++){
+            if(miningAttributesList[_gaimID][i].attribute == _Attribute){
+                miningAttributesList[_gaimID][i] = ams;
+                break;
+            }
+        }
+    }
+    
+    function isValidGameID(uint256 _gaimID) public view returns(bool){
+        return isSetGameID[_gaimID];
+    }
+    
+    function getCheckListLength(uint256 _gaimID) public view returns(uint256){
+        return miningAttributesList[_gaimID].length;
+    }
+    
+    function getAttributesVaule(uint256 _gaimID,uint256 _index) public view returns(uint256 _Attribute,uint256 _vaule){
+        _Attribute = miningAttributesList[_gaimID][_index].attribute;
+        _vaule = miningAttributesList[_gaimID][_index].vaule;
+    }
+    
+}
